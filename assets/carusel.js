6b667a980ffbedca880f1fe6846c5605df7993b6
osama2kabdullah <abdullah21673@hotmail.com>
2023-05-06 16:42:23 +0600

    #4 issue solved

--- assets/carusel.js
+++ assets/carusel.js
@@ -1,36 +1,26 @@
 const plusBtn = document.getElementById("plusBtn");
 const minusBtn = document.getElementById("minusBtn");
-const banners = document.getElementsByClassName('banner');
-let currentSlide = 0;
+const slides = document.getElementsByClassName("slide");
+let counter = 0;
 
-banners[1].style.display = "none";
-banners[2].style.display = "none";
+for (let i = 0; i < slides.length; i++) {
+  const slide = slides[i];
+  slide.style.left = `${i * 100}%`;
+}
 
-plusBtn.addEventListener('click', function(){
-    if(currentSlide === 2){
-        currentSlide = 0;
-    }else {
-        currentSlide = currentSlide + 1;
-    }
-    slide(currentSlide);
-})
-minusBtn.addEventListener('click', function(){
-    if(currentSlide === 0){
-        currentSlide = 2
-    }else {
-        currentSlide = currentSlide -1
-    }
-    slide(currentSlide);
-})
+plusBtn.addEventListener("click", function () {
+  counter = counter - 1;
+  slideImage();
+});
 
-function slide(index) {
-    for (let i = 0; i < banners.length; i++) {
-        const element = banners[i];
-        if(index === i){
-            element.style.display = "block";
-            console.log(element);
-        }else {
-            element.style.display = "none";
-        }
-    }
+minusBtn.addEventListener("click", function () {
+  counter = counter + 1;
+  slideImage();
+});
+
+function slideImage() {
+  for (let i = 0; i < slides.length; i++) {
+    const slide = slides[i];
+    slide.style.transform = `translateX(${ counter * 100 }%)`;
+  }
 }
